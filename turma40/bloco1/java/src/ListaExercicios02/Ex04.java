fc594b1e51805369af95731c31702d1167fdbe77
robertwtm <robertwtavares@gmail.com>
2021-11-10 13:31:29 -0300

    Ultimo ex do dia Classe Ex04

--- turma40/bloco1/java/src/ListaExercicios02/Ex04.java
+++ turma40/bloco1/java/src/ListaExercicios02/Ex04.java
@@ -1,5 +1,26 @@
+// 4- Faça um programa em que permita a entrada de um número qualquer e exiba se este número é par ou ímpar. Se for par exiba também a raiz quadrada do mesmo; se for ímpar exiba o número elevado ao quadrado.
 package ListaExercicios02;
+
+import java.util.Scanner;
 
 public class Ex04 {
 
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		double numero;
+		
+		System.out.println("Digite um numero: ");
+		numero = scan.nextDouble();
+		
+		if (numero%2 == 0) {
+			System.out.println("O numero digitado é Par: " +numero);
+			System.out.println("Raiz quadrada: " +Math.sqrt(numero)); 
+		} else {
+			System.out.println("Numero Impar!");
+		}
+		
+		
+	}
 }
