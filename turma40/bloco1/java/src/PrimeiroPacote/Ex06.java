6f5603c40cbf8b82e7db55342ef2639e92e7c8f5
robertwtm <robertwtavares@gmail.com>
2021-11-11 15:15:52 -0300

    Class Ex06 criada para a solução do ultimo ex do dia: // 6- Escrever um programa que receba vários números inteiros no teclado. E no final imprimir a média dos números múltiplos de 3. Para sair digitar 0(zero).(DO...WHILE)

---
+++ turma40/bloco1/java/src/PrimeiroPacote/Ex06.java
@@ -0,0 +1,29 @@
+// 6- Escrever um programa que receba vários números inteiros no teclado. E no final imprimir a média dos números múltiplos de 3. Para sair digitar 0(zero).(DO...WHILE)
+package PrimeiroPacote;
+
+import java.util.Scanner;
+
+public class Ex06 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int numero = 0, para = 10, soma = 0, media = 0, contador = 0;
+
+		do {
+			System.out.print("Digite um numero: ");
+			numero = scan.nextInt();
+
+			if (numero == 0) {
+				para = 0;
+			} else if (numero % 3 == 0) {
+				soma += numero;
+				contador++;
+			}
+
+		} while (para != 0);
+
+		media = (soma / contador);
+
+		System.out.println("Soma dos numeros digitados: " + media);
+
+	}
+}
