94e8853d20f1865ef2f86e35cc0cd4cce941639b
robertwtm <robertwtavares@gmail.com>
2021-11-11 11:26:26 -0300

    Class Repeticao1 criada para exemplo de laço de repetição usando o for atualizado

--- turma40/bloco1/java/src/PrimeiroPacote/Repeticao1.java
+++ turma40/bloco1/java/src/PrimeiroPacote/Repeticao1.java
@@ -2,13 +2,13 @@ package PrimeiroPacote;
 
 public class Repeticao1 {
 	public static void main(String[] args) {
-		
-		int x, soma=0;
-		
-		for (x=1;x<=10;x++) {
-			System.out.print(" "+x);
+
+		int x, soma = 0;
+
+		for (x = 1; x <= 10; x++) {
+			System.out.print(" " + x);
 			soma += x;
 		}
-		System.out.println("\nSoma total: "+soma);
+		System.out.println("\nSoma total: " + soma);
 	}
 }
