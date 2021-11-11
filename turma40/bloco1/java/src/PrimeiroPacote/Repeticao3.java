426d1fa6c07e8e7a6773a7dae45c1761bb0574bb
robertwtm <robertwtavares@gmail.com>
2021-11-11 11:55:23 -0300

    Class Repeticao3 criada para vermos o metodo de repeticao usando o do

---
+++ turma40/bloco1/java/src/PrimeiroPacote/Repeticao3.java
@@ -0,0 +1,24 @@
+package PrimeiroPacote;
+
+import java.util.Scanner;
+
+public class Repeticao3 {
+	public static void main(String[] args) {
+		int idade;
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Entre com a sua idade: ");
+		idade = scan.nextInt();
+
+		do {
+			System.out.print("\nSua idade é: " + idade+" anos");
+			if (idade >= 18) {
+				System.out.print("\nVoce é de maior");
+			} else {
+				System.out.print("\nVoce é de menor");
+			}
+			System.out.print("\nEntre com a sua idade: ");
+			idade = scan.nextInt();
+		} while (idade >= 1);
+	}
+}
