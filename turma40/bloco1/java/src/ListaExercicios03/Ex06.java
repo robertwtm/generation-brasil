ca8f5c822d3f2e1ada43608a36818032a0dc742f
robertwtm <robertwtavares@gmail.com>
2021-11-11 15:19:35 -0300

    Class Ex06 criada para a solução do ultimo ex do dia: // 6- Escrever um programa que receba vários números inteiros no teclado. E no final imprimir a média dos números múltiplos de 3. Para sair digitar 0(zero).(DO...WHILE)

--- turma40/bloco1/java/src/ListaExercicios03/Ex06.java
+++ turma40/bloco1/java/src/ListaExercicios03/Ex06.java
@@ -1,5 +1,5 @@
 // 6- Escrever um programa que receba vários números inteiros no teclado. E no final imprimir a média dos números múltiplos de 3. Para sair digitar 0(zero).(DO...WHILE)
-package PrimeiroPacote;
+package ListaExercicios03;
 
 import java.util.Scanner;
 
